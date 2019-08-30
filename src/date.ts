8dff25de8ac1d9ae566af9e052b46768f99dc09d
wuyx <wuyx@yunjiglobal.com>
2019-08-30 16:00:15 +0800

    fix(date):改==为===

--- src/date.ts
+++ src/date.ts
@@ -22,6 +22,6 @@ export function dateFormat(time:any, fmt:string): string {
     fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
   for (var k in o)
     if (new RegExp('(' + k + ')').test(fmt))
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
   return fmt;
 }
