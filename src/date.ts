5730ce03c42ffa81dede02095d45bbf04456a026
wuyx <wuyx@yunjiglobal.com>
2019-08-30 16:05:16 +0800

    style(date):语法易读

--- src/date.ts
+++ src/date.ts
@@ -18,10 +18,13 @@ export function dateFormat(time:any, fmt:string): string {
     'q+': Math.floor((d.getMonth() + 3) / 3), //季度
     'S': d.getMilliseconds(), //毫秒
   };
-  if (/(y+)/.test(fmt))
+  if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
-  for (var k in o)
-    if (new RegExp('(' + k + ')').test(fmt))
+  }
+  for (let k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
       fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    }
+  }
   return fmt;
 }
